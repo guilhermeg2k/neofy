8f46584848645c50d14a1cf587d8c05a5d620602
guilhermeg2k <guilhermen2k@gmail.com>
2021-06-22 10:20:25 -0300

    new feature: nnadded a label to the left-menu icons when the mouse its over them

--- components/LeftBar.tsx
+++ components/LeftBar.tsx
@@ -1,35 +1,67 @@
+import { useState } from 'react';
 import styles from "../styles/components/LeftBar.module.scss";
 import {
-    Home,
-    Search,
-    LibraryMusic,
-    Audiotrack,
-    QueueMusic,
-    Album
+  Home,
+  Search,
+  LibraryMusic,
+  Audiotrack,
+  QueueMusic,
+  Album
 } from "@material-ui/icons";
-export default function LeftBar(){
-    return (
-        <nav className={styles.leftBarContainer}>
-            <div className={styles.leftBarWrapper}>
-                <div className={styles.iconButton}>
-                    <Home />
-                </div>
-                <div className={styles.iconButton}>
-                    <Search />
-                </div>
-                <div className={styles.iconButton}>
-                    <LibraryMusic />
-                </div>
-                <div className={styles.iconButton}>
-                    <Audiotrack />
-                </div>
-                <div className={styles.iconButton}>
-                    <QueueMusic />
-                </div>
-                <div className={styles.iconButton}>
-                    <Album />
-                </div>
-            </div>
-        </nav>
-    )
+export default function LeftBar() {
+  const [hideLabel, setHideLabel] = useState(true);
+  return (
+    <nav className={styles.leftBarContainer}>
+      <div className={styles.leftBarWrapper}>
+        <div
+          className={styles.iconButton}
+          onMouseEnter={() => setHideLabel(false)}
+          onMouseLeave={() => setHideLabel(true)}
+        >
+          <Home />
+          <span className={hideLabel ? "hidden" : ""}>Home</span>
+        </div>
+        <div 
+          className={styles.iconButton} 
+          onMouseEnter={() => setHideLabel(false)} 
+          onMouseLeave={() => setHideLabel(true)}
+        >
+          <Search />
+          <span className={hideLabel ? "hidden" : ""}>Search</span>
+        </div>
+        <div 
+          className={styles.iconButton} 
+          onMouseEnter={() => setHideLabel(false)} 
+          onMouseLeave={() => setHideLabel(true)}
+        >
+          <LibraryMusic />
+          <span className={hideLabel ? "hidden" : ""}>Library</span>
+        </div>
+        <div 
+          className={styles.iconButton} 
+          onMouseEnter={() => setHideLabel(false)} 
+          onMouseLeave={() => setHideLabel(true)}
+        >
+          <Audiotrack />
+          <span className={hideLabel ? "hidden" : ""}>Songs</span>
+        </div>
+        <div 
+          className={styles.iconButton} 
+          onMouseEnter={() => setHideLabel(false)} 
+          onMouseLeave={() => setHideLabel(true)}
+        >
+          <QueueMusic />
+          <span className={hideLabel ? "hidden" : ""}>Playlists</span>
+        </div>
+        <div 
+          className={styles.iconButton} 
+          onMouseEnter={() => setHideLabel(false)} 
+          onMouseLeave={() => setHideLabel(true)}
+        >
+          <Album />
+          <span className={hideLabel ? "hidden" : ""}>Albums</span>
+        </div>
+      </div>
+    </nav>
+  )
 }
