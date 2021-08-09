220e4b190a44d70aef3a08a9e69ea3c0c61736aa
guilhermeg2k <guilhermen2k@gmail.com>
2021-08-09 12:24:51 -0300

    fix: the suggestion algorithm is not repeating items anymore

--- contexts/user.tsx
+++ contexts/user.tsx
@@ -62,26 +62,81 @@ export function UserProvider({ children }: UserProviderProps) {
     return await spotifyAPI.getUserRecentlyPlayedTracks(limit);
   }
 
+  function generateOption(optionsHistoric: Array<number>, selectionsHistoric: Array<Array<number>>): number {
+    let option = Math.floor(Math.random() * 3)
+    let isOptionFullySuggested = false;
+
+    if (optionsHistoric.length > 0) {
+
+      do {
+        option = Math.floor(Math.random() * 3);
+        switch (option) {
+          case 0:
+            if (playlists.length === selectionsHistoric[0].length) {
+              isOptionFullySuggested = true;
+            }
+            break;
+          case 1:
+            if (savedAlbums.length === selectionsHistoric[1].length) {
+              isOptionFullySuggested = true;
+            }
+          case 2:
+            if (savedTracks.length === selectionsHistoric[2].length) {
+              isOptionFullySuggested = true;
+            }
+        }
+
+      } while (optionsHistoric[optionsHistoric.length - 1] === option || isOptionFullySuggested);
+
+    }
+
+    return option;
+  }
+
+  function generateSelection(option: number, selectionsHistoric: Array<Array<number>>): number {
+    let selection: number;
+    let isAlreadySelected: Boolean;
+
+    do {
+      selection = Math.floor(Math.random() * 20);
+      isAlreadySelected = selectionsHistoric[option].some(item => item == selection);
+    } while (isAlreadySelected);
+
+    return selection;
+  }
+
   function generateSuggestions(max = 6): Array<Suggestion> {
     if (playlists.length > 0 && savedAlbums.length > 0 && savedTracks.length > 0) {
       const suggestions = Array<Suggestion>();
+      const optionsHistoric = Array<number>();
+      const selectionsHistoric = Array<Array<number>>();
+
+      for (let i = 0; i < 3; i++) {
+        selectionsHistoric.push([]);
+      }
+
       for (let i = 0; i < max; i++) {
-        const option = Math.floor(Math.random() * 3);
-        const selection = Math.floor(Math.random() * 20);
+        const option = generateOption(optionsHistoric, selectionsHistoric);
+        const selection = generateSelection(option, selectionsHistoric);
+
+        optionsHistoric.push(option);
         switch (option) {
           case 0:
+            selectionsHistoric[0].push(selection);
             suggestions.push({
               item: playlists[selection % playlists.length],
               type: "playlist"
             });
             break;
           case 1:
+            selectionsHistoric[1].push(selection);
             suggestions.push({
               item: savedAlbums[selection % savedAlbums.length],
               type: "album"
             });
             break;
           case 2:
+            selectionsHistoric[2].push(selection);
             suggestions.push({
               item: savedTracks[selection % savedTracks.length],
               type: "track"
